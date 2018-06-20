94311e0fbfa92b0bde4d0991fb171d0c6c73d0b3
--global <shimizacken@gmail.com>
2018-06-20 21:31:17 +0300

    sass integratred

--- src/components/portal/index.jsx
+++ src/components/portal/index.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
+import styles from './styles.scss';
 
 export default class MainContainer extends Component {
 
     render() {
-
+        console.log(styles);
+        
         return(
-            <div>
+            <div
+                className={styles.root}
+            >
                 Hello app!
             </div>
         );
---
+++ webpack/webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const extractSASS = new ExtractTextPlugin('portal.css');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: './src/index.html',
+  filename: './index.html'
+});
+
+module.exports = {
+  entry: [
+    'babel-polyfill',
+    './src/app.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.webpack.js', '.web.js', '.json'],
+    modules: ['node_modules']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: extractSASS.extract({
+            fallback: ['style-loader'], // translates CSS into CommonJS
+            use: [
+                {
+                    loader: 'css-loader',
+                    options: {
+                        modules: true,
+                        localIdentName: '[local]--[hash:base64:5]',
+                        sourceMap: true,
+                        minimize: true,
+                        camelCase: true
+                    }
+                },
+                'sass-loader'
+            ] // compiles sass to CSS
+        })
+      }
+    ]
+  },
+  plugins: [
+    extractSASS,
+    htmlPlugin
+  ]
+};
