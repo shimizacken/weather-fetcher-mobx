8e81e7fde0b361a61a1d8034f823b67906ddb95b
--global <shimizacken@gmail.com>
2018-06-22 23:09:44 +0300

    units fix

--- src/services/openweathermap/utils.js
+++ src/services/openweathermap/utils.js
@@ -1,9 +1,11 @@
+import { units } from './units';
+
 export const buildApiUrl = (token, unit) => {
 
-    unit = unit ? `&unit=${unit}` : '';
+    unit = unit ? `&units=${unit}` : '';
 
     return cityName => 
-            `http://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(cityName)}${unit}&appid=${token}`;
+            `http://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(cityName)}&appid=${token}${unit}`;
 }
 
 export const buildIconUrl = icon => 
